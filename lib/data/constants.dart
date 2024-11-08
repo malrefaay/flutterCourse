61c4b4d4948b05889692ddc6525ea295b7e00cd1
Ahmed Wahba <a.tarek360@gmail.com>
2024-11-08 14:38:56 +0200

    dynamic apiBaseUrl

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,1 +1,11 @@
-const apiBaseUrl = "http://10.0.2.2:8080";
+import 'dart:io';
+
+String get apiBaseUrl {
+  if (Platform.isIOS) {
+    return 'http://127.0.0.1:8080';
+  } else if (Platform.isAndroid) {
+    return 'http://10.0.2.2:8080';
+  } else {
+    return 'http://localhost:8080';
+  }
+}
